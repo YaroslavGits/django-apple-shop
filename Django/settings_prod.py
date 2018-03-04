964f192caa3bd2d9f584997939afc64f79acaa3c
root <slavik199900@gmail.com>
2018-03-04 15:25:39 +0200

    change database

--- Django/settings_prod.py
+++ Django/settings_prod.py
@@ -4,11 +4,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database',
-        'USER': 'usershop',
-        'PASSWORD': 'security_database_root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
